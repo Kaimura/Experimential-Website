ef6e9d4036bb8d188fc90195736fa64a5295cefc
Daniel Kerata <danielkerata@gmx.de>
2019-02-14 22:08:17 +0100

    Website mobile first Design now and added flip card section - first try with keyframe animation

--- JS/main.js
+++ JS/main.js
@@ -9,7 +9,7 @@
  // import {isInViewPort} from "../JS/Utils/Utils"; not supported without transpiler as babel etc
 
     isIntersecting(document.querySelector('#myDiv')); //load from global space instead (since import not working yet)
-    // observer.observe(document.querySelector('#myDiv'));
+   // observer.observe(document.querySelector('#myDiv'));
 
     // isIntersecting(document.querySelector(".space"));
     // observer.observe(document.querySelector(".space"));
