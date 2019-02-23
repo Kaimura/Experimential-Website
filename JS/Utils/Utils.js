70eaa53baf3112b8a5ef7cf522dfb515012c23da
Daniel Kerata <danielkerata@gmx.de>
2019-02-24 00:17:54 +0100

    Added small changes to images and stylings.

--- JS/Utils/Utils.js
+++ JS/Utils/Utils.js
@@ -3,33 +3,61 @@
 if ('IntersectionObserver' in window &&
     'IntersectionObserverEntry' in window &&
     'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
-  function isIntersecting(elementToSpot) {
-      let options = {
-          root: null,
-          rootMargin: '0px',
-          threshold: 0
-        };
+      var isIntersecting = function(elementToSpot) {
+        let options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0
+          };
+          
+          let callback = function(entries, observer) { 
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    entry.target.classList.add("visited");
+                }
+                // Each entry describes an intersection change for one observed
+                // target element:
+                //   entry.boundingClientRect
+                //   entry.intersectionRatio
+                //   entry.intersectionRect
+                //   entry.isIntersecting
+                //   entry.rootBounds
+                //   entry.target
+                //   entry.time
+            });
+          };
+          
+          let observer = new IntersectionObserver(callback, options);//
+          observer.observe(elementToSpot);
+    };
+
+  // function isIntersecting(elementToSpot) {
+  //     let options = {
+  //         root: null,
+  //         rootMargin: '0px',
+  //         threshold: 0
+  //       };
         
-        let callback = function(entries, observer) { 
-          entries.forEach(entry => {
-              if(entry.isIntersecting) {
-                  entry.target.classList.add("visited");
-              }
-              // Each entry describes an intersection change for one observed
-              // target element:
-              //   entry.boundingClientRect
-              //   entry.intersectionRatio
-              //   entry.intersectionRect
-              //   entry.isIntersecting
-              //   entry.rootBounds
-              //   entry.target
-              //   entry.time
-          });
-        };
+  //       let callback = function(entries, observer) { 
+  //         entries.forEach(entry => {
+  //             if(entry.isIntersecting) {
+  //                 entry.target.classList.add("visited");
+  //             }
+  //             // Each entry describes an intersection change for one observed
+  //             // target element:
+  //             //   entry.boundingClientRect
+  //             //   entry.intersectionRatio
+  //             //   entry.intersectionRect
+  //             //   entry.isIntersecting
+  //             //   entry.rootBounds
+  //             //   entry.target
+  //             //   entry.time
+  //         });
+  //       };
         
-        let observer = new IntersectionObserver(callback, options);//
-        observer.observe(elementToSpot);
-  }
+  //       let observer = new IntersectionObserver(callback, options);//
+  //       observer.observe(elementToSpot);
+  // }
 }
 else {
   console.log("Intersection Observer is not supported");
