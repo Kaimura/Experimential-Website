3c46c3943f814d8e823a371799af15a2c442712f
Daniel Kerata <danielkerata@gmx.de>
2019-03-07 16:27:24 +0100

    Intersection Observer now used for navbar class toggling instead of slideInLeft animation - also fixed navbar and added attributes to html in order to make new Intersection Observer usage work

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,47 @@
 /*jshint esversion: 6 */
 
 /* Intersection Observer not working in opera, safari, firefox android, opera android, safari ios */
-  
- // import {isInViewPort} from "../JS/Utils/Utils"; not supported without transpiler as babel etc
+/*jshint esversion: 6 */
 
-    isIntersecting(document.querySelector('#myDiv')); //load from global space instead (since import not working yet)
-   // observer.observe(document.querySelector('#myDiv'));
+if ('IntersectionObserver' in window &&
+    'IntersectionObserverEntry' in window &&
+    'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
+      var isIntersecting = function(elementToSpot) {
+        let options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.8 //trigger only when at least 80% of element is visible
+          };
+          
+          let callback = function(entries, observer) {
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    // entry.target.classList.add("visited");
+                    document.querySelector('a.active') ? document.querySelector('a.active').classList.remove('active') : null
+                    document.getElementsByName(entry.target.id)[0].classList.add('active');
+                }
+                // Each entry describes an intersection change for one observed
+                // target element:
+                //   entry.boundingClientRect
+                //   entry.intersectionRatio
+                //   entry.intersectionRect
+                //   entry.isIntersecting
+                //   entry.rootBounds
+                //   entry.target
+                //   entry.time
+            });
+          };
+          
+          let observer = new IntersectionObserver(callback, options);
+          elementToSpot.forEach(element => {
+            observer.observe(element);
+          })
+          // observer.observe(elementToSpot);
+    };
+}
+else {
+  console.log("Intersection Observer is not supported");
+  // polyfill
+}
 
-    // isIntersecting(document.querySelector(".space"));
-    // observer.observe(document.querySelector(".space"));
+isIntersecting(document.getElementsByName("section"));
